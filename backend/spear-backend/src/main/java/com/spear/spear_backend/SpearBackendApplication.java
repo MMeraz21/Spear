86c1847648a5023d3c420421e7dd22e3b3a14940
Manuel Meraz <manuelmeraz2190@gmail.com>
2024-12-04 20:19:18 -0600

    starting backend

---
+++ backend/spear-backend/src/main/java/com/spear/spear_backend/SpearBackendApplication.java
@@ -0,0 +1,13 @@
+package com.spear.spear_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpearBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpearBackendApplication.class, args);
+	}
+
+}
---
+++ backend/spear-backend/src/test/java/com/spear/spear_backend/SpearBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spear.spear_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpearBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
