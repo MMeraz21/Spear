e9cc56aeae998bb16064eafa1897cf4a7febd3a4
Manuel Meraz <manuelmeraz2190@gmail.com>
2024-12-08 23:48:42 -0600

    typescript changes

--- frontend/spear-mobile/App.tsx
+++ frontend/spear-mobile/App.tsx
@@ -1,14 +1,17 @@
+import React from 'react'; 
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+const App: React.FC = () => {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
-}
+};
+
+export default App;
 
 const styles = StyleSheet.create({
   container: {
