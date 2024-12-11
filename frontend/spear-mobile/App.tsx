1a3d045e66230ca2ae3fb1f1fa53fffe9be3229a
Manuel Meraz <manuelmeraz2190@gmail.com>
2024-12-11 16:24:55 -0600

    google sign in done

--- frontend/spear-mobile/App.tsx
+++ frontend/spear-mobile/App.tsx
@@ -13,25 +13,55 @@ type UserInfo = {
   picture: string;
 };
 
-
-
 WebBrowser.maybeCompleteAuthSession();
 
 const App: React.FC = () => {
-  const [UserInfo, setUserInfo] = React.useState(null);
+  const [UserInfo, setUserInfo] = useState<UserInfo | null>(null);
 
   const [request, response, promptAsync] = Google.useAuthRequest({
     iosClientId: process.env.IOS_CLIENT_ID
-  })
+  });
+
+  useEffect(() => {
+    handleGoogle();
+  }, [response]);
 
   const handleSignIn = async () => {
     await promptAsync(); // Call the promptAsync function
   };
 
+  const handleGoogle = async () => {
+    const user = await AsyncStorage.getItem("@user");
+
+    if(!user){
+      if(response?.type === "success" && response.authentication?.accessToken){
+        await getUserInfo(response.authentication.accessToken);
+      }
+    }else{
+      setUserInfo(JSON.parse(user));
+    }
+  };
+
+  const getUserInfo = async (token: string) => {
+    if(!token) return;
+    try {
+      const response = await fetch(
+        "https://www.googleapis.com/userinfo/v2/me",
+        {headers: {Authorization: `Bearer ${token}`},}
+      );
+
+      const user = await response.json();
+      await AsyncStorage.setItem("@user", JSON.stringify(user));
+      setUserInfo(user);
+    } catch (error){
+      console.log(error);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <Button title = "Sign in with Googleeeee" onPress={handleSignIn}/>
+      <Text>{UserInfo ? JSON.stringify(UserInfo, null, 2) : "No user info available"}</Text>
+      <Button title = "Sign in with Google" onPress={handleSignIn}/>
     </View>
   );
 };
