228cf97f852015839a5bf6f394ed93a187aee56c
Manuel Meraz <manuelmeraz2190@gmail.com>
2024-12-10 22:37:11 -0600

    google auth

--- frontend/spear-mobile/App.tsx
+++ frontend/spear-mobile/App.tsx
@@ -1,15 +1,37 @@
-import React from 'react'; 
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, Image, TouchableOpacity, View, Alert, StyleSheet, StatusBar, Button} from 'react-native';
+import * as Google from 'expo-auth-session/providers/google';
+import * as AuthSession from 'expo-auth-session';
+import * as WebBrowser from "expo-web-browser";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+// import { IOS_CLIENT_ID, BUNDLE_IDENTIFIER } from '@env';
 
-//test2
+type UserInfo = {
+  id: string;
+  email: string;
+  name: string;
+  picture: string;
+};
 
+
+
+WebBrowser.maybeCompleteAuthSession();
 
 const App: React.FC = () => {
+  const [UserInfo, setUserInfo] = React.useState(null);
+
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    iosClientId: process.env.IOS_CLIENT_ID
+  })
+
+  const handleSignIn = async () => {
+    await promptAsync(); // Call the promptAsync function
+  };
+
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Button title = "Sign in with Googleeeee" onPress={handleSignIn}/>
     </View>
   );
 };
---
+++ frontend/spear-mobile/webpack.config.js
@@ -0,0 +1,16 @@
+// webpack.config.js
+
+const { withExpoWebpack } = require('@expo/webpack-config');
+
+// module.exports = function (env, argv) {
+//   const config = withExpoWebpack(env, argv);
+
+//   // If you want to customize the config, you can modify it here
+//   config.resolve.fallback = {
+//     ...config.resolve.fallback,
+//     crypto: require.resolve('crypto-browserify')
+//   };
+
+//   return config;
+// };
+
